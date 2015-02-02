2a88f24df7033a74e13a22c6609db83a36d1463d
Robert Greinacher <robert@3antworten.de>
2015-02-02 02:29:41 +0100

    added interactive mode as argument

--- animationController.py
+++ animationController.py
@@ -228,60 +228,62 @@ class LightAnimation(Thread):
 # ************************************************
 # non object orientated entry code goes down here:
 # ************************************************
-def startAnimationControllerThread():
+def startAnimationControllerThread(interactiveMode = False):
     lightAnimation = LightAnimation()
     lightAnimation.start()
 
-    entered = ''
-    while True:
-        entered = input('call a function: ')
-        if entered == 'glow':
-            print('showAnimation(randomGlow)')
-            lightAnimation.showAnimation('randomGlow')
-        elif entered == 'circle':
-            print('showAnimation(pulsingCircle)')
-            lightAnimation.showAnimation('pulsingCircle')
-        elif entered == 'clock':
-            print('showAnimation(binaryClock)')
-            lightAnimation.showAnimation('binaryClock')
-        elif entered == 'cc':
-            print('showAnimation(binaryClockWithPulsingCircle)')
-            lightAnimation.showAnimation('binaryClockWithPulsingCircle')
-        elif entered == 'mono':
-            print('showAnimation(monoColor)')
-            lightAnimation.showAnimation('monoColor')
-        elif entered == 'fade':
-            seconds = int(input('seconds to fade out: '))
-            print('showAnimation(fadeOut, ' + str(seconds) + ')')
-            lightAnimation.showAnimation('fadeOut', seconds)
-        elif entered == 'color':
-            color = input('hex color: ')
-            color = int(color, 16)
-            print('setBasisColorAsHex(' + str(color) + ')')
-            lightAnimation.setBasisColorAsHex(color)
-        elif entered == 'ccolor':
-            color = input('hex color: ')
-            color = int(color, 16)
-            print('setBinaryClockColor(' + str(color) + ')')
-            lightAnimation.setBinaryClockColor(color)
-        elif entered == 'lightness':
-            lightness = input('lightness [0..100]: ')
-            lightness = int(lightness) / 100
-            print('setBasisLightness(' + str(lightness) + ')')
-            lightAnimation.setBasisLightness(lightness)
-        elif entered == 'clightness':
-            lightness = input('lightness [0..100]: ')
-            lightness = int(lightness) / 100
-            print('setBinaryClockLightness(' + str(lightness) + ')')
-            lightAnimation.setBinaryClockLightness(lightness)
-        else:
-            print('unrecognized command!')
+    if interactiveMode:
+        entered = ''
+        while True:
+            entered = input('call a function: ')
+            if entered == 'glow':
+                print('showAnimation(randomGlow)')
+                lightAnimation.showAnimation('randomGlow')
+            elif entered == 'circle':
+                print('showAnimation(pulsingCircle)')
+                lightAnimation.showAnimation('pulsingCircle')
+            elif entered == 'clock':
+                print('showAnimation(binaryClock)')
+                lightAnimation.showAnimation('binaryClock')
+            elif entered == 'cc':
+                print('showAnimation(binaryClockWithPulsingCircle)')
+                lightAnimation.showAnimation('binaryClockWithPulsingCircle')
+            elif entered == 'mono':
+                print('showAnimation(monoColor)')
+                lightAnimation.showAnimation('monoColor')
+            elif entered == 'fade':
+                seconds = int(input('seconds to fade out: '))
+                print('showAnimation(fadeOut, ' + str(seconds) + ')')
+                lightAnimation.showAnimation('fadeOut', seconds)
+            elif entered == 'color':
+                color = input('hex color: ')
+                color = int(color, 16)
+                print('setBasisColorAsHex(' + str(color) + ')')
+                lightAnimation.setBasisColorAsHex(color)
+            elif entered == 'ccolor':
+                color = input('hex color: ')
+                color = int(color, 16)
+                print('setBinaryClockColor(' + str(color) + ')')
+                lightAnimation.setBinaryClockColor(color)
+            elif entered == 'lightness':
+                lightness = input('lightness [0..100]: ')
+                lightness = int(lightness) / 100
+                print('setBasisLightness(' + str(lightness) + ')')
+                lightAnimation.setBasisLightness(lightness)
+            elif entered == 'clightness':
+                lightness = input('lightness [0..100]: ')
+                lightness = int(lightness) / 100
+                print('setBinaryClockLightness(' + str(lightness) + ')')
+                lightAnimation.setBinaryClockLightness(lightness)
+            else:
+                print('unrecognized command!')
 
 # check if this code is run as a module or was included into another project
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description = "Controller program for Arduino powerd RGB-LED strand")
     parser.add_argument("-d", "--daemon", action = "store_true", dest = "daemon", help = "enables daemon mode")
     parser.add_argument("-v", "--verbose", action = "store_true", dest = "verbose", help = "enables verbose mode")
+    parser.add_argument("-i", "--interactive", action = "store_true", dest = "interactive", help = "enables interactive mode")
     parser.add_argument("-fps", "--updates", action = "store_true", dest = "updates", help = "display USB update rate per second")
     args = parser.parse_args()
 
@@ -296,4 +298,4 @@ if __name__ == "__main__":
         daemon = Daemonize(app='SleepServer Daemon', pid=pidFile, action=startAnimationControllerThread)
         daemon.start()
     else:
-        startAnimationControllerThread()
+        startAnimationControllerThread(args.interactive and not SHOW_UPDATE_RATE)
